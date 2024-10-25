c22c8b54d2213858f1704f45c0977a08a889a06d
pa <pa@arista.com>
2024-10-25 11:06:56 +0200

    .

--- containers/cvp-base/.devcontainer/postCreate.sh
+++ containers/cvp-base/.devcontainer/postCreate.sh
@@ -2,4 +2,14 @@
 
 set +e
 
+sudo ip link add name br0 type bridge
+sudo ip link set br0 type bridge stp_state 0
+sudo ip link set br0 type bridge vlan_stats_per_port 1
+sudo ip addr add 192.168.122.1/24 dev br0
+sudo ip link set dev br0 up
+sudo mkdir /etc/qemu
+echo "allow all" | sudo tee /etc/qemu/bridge.conf
+
 ardl get cvp --format kvm --version ${CVP_VERSION}
+
+tar -xzvf cvp-2024*
